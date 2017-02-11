4028ceb9b56ba6314a62efc0e476d189aaeee882
José Carlos Castillo <jc@MacBook-Pro-de-Jose-2.local>
2017-02-11 19:05:50 +0100

    Added a new feature to allow re-label the current image.

--- shark_labelling.m
+++ shark_labelling.m
@@ -2,7 +2,7 @@
 %%%%%%% SELECT AREA TO ANALYZE
 %
 % Set image_path to the name to the image to analyze.
-% When the first image appears, select the are that contains sharks. 
+% When the first image appears, select the are that contains sharks.
 % Then, right click on the edge of the rectangle and select Crop Image
 %
 
@@ -39,25 +39,46 @@ I = imcrop(shark_crop,[1,1 offset,offset]);
 close all
 imagesc(I);axis equal;axis tight
 hold on;
+disp('Press b and ENTER to label again the current image');
 next_subimage=false;
-sharks_labelled = [0,0,0,0,0,0,0,0];
+sharks_labeled = [0,0,0,0,0,0,0,0];
+sharks_labeled_temp = [];
+old_cell_row = 1;
+old_cell_col = 1;
 
-
+%%% LETS TRY ADDING REWIND CAPABILITIES
+%%% PRESS b and ENTER to drop the last image labeling and label it again.
 
 while end_labelling
     
-    shark_pose = ginput(2);
-    
+    [sx, sy, buttons] = ginput(2);
+    %shark_pose = ginput(2);
+    shark_pose = [sx,sy];
     %No more sharks in subimage
     [n_points,~] = size(shark_pose);
+    
     if(n_points< 2)
-       [I,cell_row,cell_col] = getSubImage(shark_crop,cell_row,cell_col,offset);
-       if(I==0)
-           break;%End of image reached
-       end
-       close;
-       imagesc(I);axis equal;axis tight 
-       hold on;
+        buttons
+        if(n_points>0 && buttons(end)==98) % letters B or b, retry
+            if(old_cell_row == 1)
+                I = imcrop(shark_crop,[1,1 offset,offset]);
+            else               
+                [I,cell_row,cell_col] = getSubImage(shark_crop,old_cell_row,old_cell_col,offset);
+            end
+        else
+            old_cell_row = cell_row;
+            old_cell_col  = cell_col;
+            [I,cell_row,cell_col] = getSubImage(shark_crop,cell_row,cell_col,offset);
+            sharks_labeled = [sharks_labeled ; sharks_labeled_temp];
+            if(I==0)
+                break; % End of image reached
+            end
+        end
+        shark_labeled_temp = [];
+        close;
+        imagesc(I);axis equal;axis tight
+        hold on;
+        
     else
         shark_tail = shark_pose(1,:);
         shark_head = shark_pose(2,:);
@@ -70,18 +91,17 @@ while end_labelling
         shark_head(2) = shark_head(2) + cell_row;
         % For each labelled shark the two points (head and tail), central
         % point and magnitude of the vector are stored.
-        sharks_labelled = [sharks_labelled ; [shark_tail, shark_head, x, y,u,v]];
+        sharks_labeled_temp = [sharks_labeled_temp ; [shark_tail, shark_head, x, y,u,v]];
     end
     
 end
 
 %Delete first row
-sharks_labelled = sharks_labelled(2:end,:);
-printVectors(shark_img,sharks_labelled,rect(1),rect(2));
+sharks_labeled = sharks_labeled(2:end,:);
+printVectors(shark_img,sharks_labeled,rect(1),rect(2));
 
 save(save_info_path);
 %knnsearch
 
 
 
-    
